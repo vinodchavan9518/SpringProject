75e790e9f3d0a18116a1199331bfe79fcf5a808f
vinodchavan9518 <vinodchavan9518@gmail.com>
2026-05-19 20:57:30 +0530

    first commit

---
+++ src/com/vinod/comps/BulletBike.java
@@ -0,0 +1,10 @@
+package com.vinod.comps;
+
+public class BulletBike  implements Bike{
+	
+	public void drive()
+	{
+		IO.println("BulletBike.drive()::driving BulletBike(Royal Enfield_bullet)");
+	}
+
+}
---
+++ src/com/vinod/comps/SportSBike.java
@@ -0,0 +1,9 @@
+ package com.vinod.comps;
+
+public class SportSBike  implements Bike{
+	public void drive()
+	{
+		IO.println("SportingBike.drive()::driving SpottsBike(kawasaki-Ninja)");
+	}
+
+}
